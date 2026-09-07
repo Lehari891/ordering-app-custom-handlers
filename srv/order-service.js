c1f80f3dd22a1c14a90117337c33b0e20e384eb8
Leharika Sri Chalapati <chalapati-leharika.sri@capgemini.com>
2026-09-07 07:23:06 +0000

    custom handlers

---
+++ srv/order-service.js
@@ -0,0 +1,85 @@
+const cds = require('@sap/cds');
+const { Books } = cds.entities;
+
+module.exports = srv => {
+    const db = cds.db;
+    //READ : SELECT
+    //CREATE : INSERT
+    //UPDATE : UPDATE
+    //DELETE : DELETE
+
+    srv.on('READ', 'OrderSet', async req => {
+        results = [];
+        results = await db.run([
+            SELECT.from(Orders).where({ ID: req.data.ID })
+        ]);
+        return results;
+    });
+
+    //BEFORE
+
+    srv.before('CREATE', 'Orders', req => {
+
+    if (req.data.totalAmount < 5680.8)    req.error(400, "totalAmount cannot be less");
+
+});
+
+
+    //CREATE
+    srv.on('CREATE', 'OrderSet', async (req, resp) => {
+        results = [];
+        results = await db.run([
+            INSERT.into(Orders).entries(req.data)
+        ])
+            .then((resolve, reject) => {
+                if (resolve)
+                    return req.data;
+                else
+                    return req.error(400, 'Failed to create');
+            })
+            .catch(err => {
+                req.error(500, 'Try after some time....');
+            })
+        return results;
+    });
+
+    //UPDATE
+    srv.on('UPDATE', 'OrderSet', async (req, resp) => {
+        results = [];
+        results = await db.run([
+            UPDATE(Books).set(req.data).where({ ID: req.data.ID })
+
+        ])
+            .then((resolve, reject) => {
+                if (resolve)
+                    return req.data;
+                else
+                    return req.error(400, 'Failed to create');
+            })
+            .catch(err => {
+                req.error(500, 'Try after some time....');
+            })
+        return results;
+    });
+
+
+    //AFTER
+
+    srv.after('UPDATE', 'OrderSet', (data, req) => {
+    console.log(`Book updated: ${data.ID}`);
+    console.log('Updated totalAmount:', data.totalAmount);
+});
+
+
+    //DELETE
+    srv.on('DELETE', 'BooksSet', async req => {
+        results = [];
+        results = await db.run([
+            DELETE.from(Books).where({ ID: req.data.ID })
+        ]);
+        return results;
+    });
+
+
+
+}
